3c1d4dc343b27a988d4765ec5b28b4c5f69194f7
learnesh <32178001+learnesh@users.noreply.github.com>
2018-03-01 23:20:01 -0600

    Create ProductItems.sql

---
+++ ProductItems.sql
@@ -0,0 +1,12 @@
+INSERT INTO products (product_name, department_name, customer_price, stock_quantity)
+VALUES 
+1, "perfume", "beauty", 800.00, 50,
+2, "iphone", "electronics", 1000, 100,
+3, "kindle", "books & audio", 80, 20
+4, "blue ray", "movies", 15, 50,
+5, "couch", "furniture", 2.00, 30,
+6, "sandals", "shoes", 8, 20,
+7, "sleeping bag", "outdoors", 10, 30,
+8, "rings", "jewelry", 5, 30,
+9, "pillow", "home-decor", 20, 10
+10, "skirt", "clothing", 100, 8
